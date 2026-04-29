e7fbd3eb131c4a2c0060c5e1eea79c272f274d38
Gaurav K <gaurav98095@gmail.com>
2026-04-29 23:48:32 +0530

    Spin Custom Subagent

--- mybot/agent/tools/__init__.py
+++ mybot/agent/tools/__init__.py
@@ -1,6 +1,7 @@
 from mybot.agent.tools.base import Tool
 from mybot.agent.tools.registry import ToolRegistry
 from mybot.agent.tools.shell import ShellTool
+from mybot.agent.tools.subagent import SubagentTool
 from mybot.agent.tools.web_search import WebSearchTool
 
-__all__ = ["Tool", "ToolRegistry", "ShellTool", "WebSearchTool"]
+__all__ = ["Tool", "ToolRegistry", "ShellTool", "SubagentTool", "WebSearchTool"]
